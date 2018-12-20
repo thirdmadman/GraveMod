f0ce3f007fd3b5344dc83e7de8b7e0eb476cf010
thirdmadman <thirdmadman@gmail.com>
2018-12-21 02:19:30 +0400

    main configs added

--- GraveMod.ino
+++ GraveMod.ino
@@ -1,5 +1,34 @@
 #include <U8g2lib.h>
 
+#define buttonFire 3
+#define buttonUp 4
+#define buttonDown 4
+#define fireMosfets 9
+#define measureMosfet 8
+#define measureBattery A0
+#define measureVoltagedivider A1
+#define lowBattery 3.7
+#define lowCriticalBattery 3.4
+#define overchargedBattery 4.35
+#define mosfetResistance 0.01
+#define voltagedividerR1 3
+#define authorName thirdmadman
+#define version 0.1
+#define isDev true
+#define powerLimit 250
+#define unsafeMode
+#define minCoilResistance 0.01
+#define maxCoilResistance 10
+
+#if ENABLED(unsafeMode)
+	#define minCoilResistance 0.0001
+	#define maxCoilResistance 1000
+	#define lowBattery 3.4
+	#define lowCriticalBattery 3.2
+#endif
+
+
+
 U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 void setup()
 {
@@ -14,10 +43,10 @@ void setup()
 	TCCR1A = TCCR1A & 0xe0 | 3;
 	TCCR1B = TCCR1B & 0xe0 | 0x09;
 //</PWM>
-
-	//Serial.begin(9600); // Use fore debug?
+	if (isDev == true) {
+		Serial.begin(9600); // Use fore debug?
+	}
   u8g2.begin();
-
 }
 
 //<Interrup>
@@ -26,6 +55,7 @@ SIGNAL(TIMER0_COMPA_vect)
 
 }
 //</Interrup>
+
 //<Loop>
 void loop()
 {
