eb2316c4f9e042d101617f9c9dd755c0bc988806
thirdmadman <thirdmadman@gmail.com>
2018-12-21 22:53:06 +0400

    some realization for drawBattery + test in main

--- src/main.cpp
+++ src/main.cpp
@@ -18,24 +18,25 @@
 #define lowBattery 3.7
 #define lowCriticalBattery 3.4
 #define overchargedBattery 4.35
+#define maxCharchedBattery 4.25
 #define mosfetResistance 0.01
 #define voltagedividerR1 3
 #define authorName "thirdmadman"
 #define version 0.1
-#define isDev false
+#define isDev true
 #define powerLimit 250
 #define unsafeMode true
 #define minCoilResistance 0.01
 #define maxCoilResistance 10
-#define spalshScreen true
+#define spalshScreen false
 #define spalshScreenDuration 2000
 
 bool wasSplash = false;
+float batteryVoltage = 3;
 unsigned long startMillis = 0;
 U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 void getSpalshScreen(void) {
-
   u8g2.firstPage();
   do {
     u8g2.setFont(u8g2_font_ncenB10_te);
@@ -60,12 +61,44 @@ void getSpalshScreen(void) {
 
 void drawBattery(float voltage) {
 
+  if (voltage>maxCharchedBattery) {
+    //don't let user use this battery
+  }
+  else if ((voltage<=maxCharchedBattery) && (voltage>lowBattery)) {
+    u8g2.drawFrame(96,4,25,10);
+    u8g2.drawBox(121, 6, 3, 6);
+    int batteryPix = floor((voltage - lowBattery) / ((maxCharchedBattery - lowBattery) / 25.0));
+		if (batteryPix > 25) {
+			batteryPix = 25;
+		}
+    u8g2.drawBox(97, 5, batteryPix, 8);
+
+  }
+  else if ((voltage<=lowBattery) && (voltage>lowCriticalBattery)) {
+    //notice - battery is low
+    u8g2.drawFrame(96,4,25,10);
+    u8g2.drawBox(121, 6, 3, 6);
+  }
+  else if ((voltage<=lowCriticalBattery) && (voltage>(lowCriticalBattery-1))) {
+    //don't let user discharge battery more
+    u8g2.drawFrame(96,4,25,10);
+    u8g2.drawBox(121, 6, 3, 6);
+  }
+  else if (voltage<=(lowCriticalBattery-1)) {
+    //check your battery - is it there?
+  }
+  u8g2.setFont(u8g2_font_ncenB08_te);
+  u8g2.setCursor(72,13);
+  u8g2.print(voltage);
+
+
 }
 
 void drawMainFrame(void) {
   u8g2.firstPage();
   do {
     u8g2.drawFrame(0,0,128,64);
+    drawBattery(batteryVoltage);
   } while (u8g2.nextPage());
 }
 
@@ -102,6 +135,15 @@ SIGNAL(TIMER0_COMPA_vect)
 void loop()
 {
 
+  if (batteryVoltage>maxCharchedBattery){
+    batteryVoltage = lowCriticalBattery;
+  }
+  else if (batteryVoltage<maxCharchedBattery) {
+    batteryVoltage+=0.01;
+  }
+
+
+  Serial.println(batteryVoltage);
 
   if ((wasSplash == false) && (spalshScreen == true) &&  (millis()-startMillis<=spalshScreenDuration)) {
     getSpalshScreen();
