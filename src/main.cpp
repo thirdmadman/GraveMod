a556cc1b625c1f44e15d2c2269e2579e235c73ca
thirdmadman <thirdmadman@gmail.com>
2018-12-28 17:24:41 +0400

    reinit dev

--- src/main.cpp
+++ src/main.cpp
@@ -1,69 +1,10 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 
-#ifdef U8X8_HAVE_HW_SPI
-#include <SPI.h>
-#endif
-#ifdef U8X8_HAVE_HW_I2C
-#include <Wire.h>
-#endif
-
-#define buttonFire 3
-#define buttonUp 4
-#define buttonDown 4
-#define fireMosfets 9
-#define measureMosfet 8
-#define measureBattery A0
-#define measureVoltagedivider A1
-#define lowBattery 3.7
-#define lowCriticalBattery 3.4
-#define overchargedBattery 4.35
-#define mosfetResistance 0.01
-#define voltagedividerR1 3
-#define authorName "thirdmadman"
-#define version 0.1
-#define isDev false
-#define powerLimit 250
-#define unsafeMode true
-#define minCoilResistance 0.01
-#define maxCoilResistance 10
-#define spalshScreen true
-#define spalshScreenDuration 2000
-
-float power = 0;
-bool wasSplash = false;
-unsigned long startMillis = 0;
 U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
-void getSpalshScreen(void) {
-
-  u8g2.firstPage();
-  do {
-    u8g2.setFont(u8g2_font_ncenB10_te);
-    u8g2.setCursor(25,14);
-    u8g2.print("Grave mod");
-
-    u8g2.setFont(u8g2_font_ncenB08_tr);
-    u8g2.setCursor(44,28);
-    u8g2.print("made by");
-
-    u8g2.setFont(u8g2_font_ncenB10_tr);
-    u8g2.setCursor(14,42);
-    u8g2.print(power);
-
-    u8g2.setFont(u8g2_font_ncenB08_tr);
-    u8g2.setCursor(36,54);
-    u8g2.print("version");
-    u8g2.setCursor(80,54);
-    u8g2.print(version);
-  } while (u8g2.nextPage());
-}
-
 void drawMainFrame(void) {
-  u8g2.firstPage();
-  do {
-    u8g2.drawFrame(0,0,128,64);
-  } while (u8g2.nextPage());
+
 }
 
 void setup()
@@ -79,11 +20,7 @@ void setup()
   TCCR1A = TCCR1A & 0xe0 | 3;
   TCCR1B = TCCR1B & 0xe0 | 0x09;
   //</PWM>
-  if (isDev == true) {
-    Serial.begin(9600); // Use fore debug?
-  }
   u8g2.begin();
-  startMillis=millis();
 }
 
 //<Interrup>
@@ -98,18 +35,10 @@ SIGNAL(TIMER0_COMPA_vect)
 //<Loop>
 void loop()
 {
-
-
-  if ((wasSplash == false) && (spalshScreen == true) &&  (millis()-startMillis<=spalshScreenDuration)) {
-    getSpalshScreen();
-  }
-  else if ((wasSplash == false) && (spalshScreen == true) &&  (millis()-startMillis>spalshScreenDuration)) {
-    wasSplash = true;
-  }
-  else {
-    drawMainFrame();
-  }
-
+  u8g2.firstPage();
+  do {
+    u8g2.drawFrame(0,0,128,64);
+  } while (u8g2.nextPage());
 
 }
 //</Loop>
