a0e768a81e39f4756e4e6e828701240f6f3871a4
thirdmadman <thirdmadman@gmail.com>
2018-12-30 18:38:35 +0400

    finally optimized!!!

--- src/main.cpp
+++ src/main.cpp
@@ -16,14 +16,14 @@ U8G2_SSD1306_128X64_NONAME_2_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 #define mosfetResistance 0.01
 #define voltagedividerR1 3
 #define authorName "thirdmadman"
-#define version 0.1
+#define version "0.1"
 #define isDev true
 #define powerLimit 250
 #define unsafeMode true
 #define minCoilResistance 0.01
 #define maxCoilResistance 10
-#define spalshScreen false
-#define spalshScreenDuration 2000
+#define spalshScreen true
+#define spalshScreenDuration 20000
 #define lowestResistance 0.015
 #define lowestResistanceUnsafe 0.0001
 #define highestResistance 15
@@ -71,40 +71,24 @@ String getBatteryState(float voltage) {
 
 void getSpalshScreen(void) {
 
-  u8g2.firstPage();
-  do {
-    u8g2.setFont(u8g2_font_ncenB10_te);
-    u8g2.setCursor(25,14);
-    u8g2.print("Grave mod");
-
+    u8g2.drawStr(35,12, "Grave mod");
+    u8g2.drawStr(42,24, "made by");
+    u8g2.setFont(u8g2_font_10x20_tr);
+    u8g2.drawStr(10,42, authorName);
     u8g2.setFont(u8g2_font_ncenB08_tr);
-    u8g2.setCursor(44,28);
-    u8g2.print("made by");
-
-    u8g2.setFont(u8g2_font_ncenB10_tr);
-    u8g2.setCursor(14,42);
-    u8g2.print(authorName);
-
-    u8g2.setFont(u8g2_font_ncenB08_tr);
-    u8g2.setCursor(36,54);
-    u8g2.print("version");
-    u8g2.setCursor(80,54);
-    u8g2.print(version);
-  } while (u8g2.nextPage());
+    u8g2.drawStr(36,56, "version");
+    u8g2.drawStr(80,56, version);
 }
 
 void drawBattery(float voltage) {
 
   String state = getBatteryState(voltage);
-  //Serial.println(state);
 
   if (state=="BATTERY_OVERCHARGED") {
     //don't let user use this battery
     u8g2.drawFrame(96,4,25,10);
     u8g2.drawBox(121, 6, 3, 6);
-    u8g2.setFont(u8g2_font_ncenB08_te);
-    u8g2.setCursor(104,13);
-    u8g2.print("!!!");
+    u8g2.drawStr(104,13,"!!!");
   }
   else if (state=="BATTERY_NORMAL") {
     u8g2.drawFrame(96,4,25,10);
@@ -114,18 +98,14 @@ void drawBattery(float voltage) {
 			batteryPix = 25;
 		}
     u8g2.drawBox(97, 5, batteryPix, 8);
-
   }
   else if (state=="BATTERY_LOW") {
     //notice - battery is low
     u8g2.drawFrame(96,4,25,10);
     u8g2.drawBox(121, 6, 3, 6);
-    u8g2.setFont(u8g2_font_ncenB08_te);
     u8g2.setCursor(72,13);
     u8g2.print(voltage);
-    u8g2.setFont(u8g2_font_ncenB08_te);
-    u8g2.setCursor(109,13);
-    u8g2.print("!");
+    u8g2.drawStr(109,13,"!");
   }
   else if (state=="BATTERY_LOWCRITICAL") {
     //don't let user discharge battery more
@@ -133,20 +113,13 @@ void drawBattery(float voltage) {
     u8g2.drawBox(121, 6, 3, 6);
     u8g2.drawLine(96,4,120,13);
     u8g2.drawLine(96,13,120,4);
-
-
   }
   else if (state == "BATTERY_NONE") {
     //check your battery - is it there?
-    u8g2.setFont(u8g2_font_ncenB08_te);
-    u8g2.setCursor(108,13);
-    u8g2.print("?");
+    u8g2.drawStr(108,13,"?");
   }
-  u8g2.setFont(u8g2_font_ncenB08_te);
   u8g2.setCursor(72,13);
   u8g2.print(voltage);
-
-
 }
 
 void drawResitance(float resistance) {
@@ -155,14 +128,13 @@ void drawResitance(float resistance) {
   //u8g2.print(setResistance);
 
   if (resistance > highestResistance) {
-    u8g2.setFont(u8g2_font_ncenB08_te);
     u8g2.setCursor(8,13);
     u8g2.print("coil none");
   }
   else if (resistance >= 0.01) {
     u8g2.setFont(u8g2_font_6x12_t_symbols);
     u8g2.drawGlyph(19, 13, 0x2126);
-    u8g2.setFont(u8g2_font_ncenB08_te);
+    u8g2.setFont(u8g2_font_ncenB08_tr);
 
     u8g2.setCursor(27,13);
     u8g2.print(resistance);
@@ -170,14 +142,13 @@ void drawResitance(float resistance) {
   else if (resistance < 0.01 && resistance > lowestResistanceUnsafe) {
     u8g2.setFont(u8g2_font_6x12_t_symbols);
     u8g2.drawGlyph(19, 13, 0x2126);
-    u8g2.setFont(u8g2_font_ncenB08_te);
+    u8g2.setFont(u8g2_font_ncenB08_tr);
     u8g2.setCursor(6,13);
     u8g2.print("m");
     u8g2.setCursor(27,13);
     u8g2.print(resistance*1000);
   }
   else if (resistance < lowestResistanceUnsafe) {
-    u8g2.setFont(u8g2_font_ncenB08_te);
     u8g2.setCursor(8,13);
     u8g2.print("coil short");
   }
@@ -186,19 +157,19 @@ void drawResitance(float resistance) {
 }
 
 void drawPower(float power) {
-  u8g2.setFont(u8g2_font_ncenB18_te);
-  u8g2.setCursor(8,13);
+  u8g2.setFont(u8g2_font_10x20_tr);
+  u8g2.setCursor(8,36);
   u8g2.print(power);
+  u8g2.setFont(u8g2_font_ncenB08_tr);
 }
 
 void drawMainFrame(void) {
-  u8g2.firstPage();
-  do {
+
     u8g2.drawFrame(0,0,128,64);
     drawBattery(batteryVoltage);
     drawResitance(coilResistance);
     drawPower(power);
-  } while (u8g2.nextPage());
+
 }
 
 
@@ -220,6 +191,7 @@ void setup(){
   }
   u8g2.begin();
   startMillis=millis();
+  u8g2.setFont(u8g2_font_ncenB08_tr);
 
 }
 
@@ -233,7 +205,8 @@ SIGNAL(TIMER0_COMPA_vect){
 
 //<Loop>
 void loop(){
-
+  u8g2.firstPage();
+  do {
   if (batteryVoltage>maxCharchedBattery+1){
     batteryVoltage = lowCriticalBattery;
 
@@ -266,6 +239,6 @@ void loop(){
     drawMainFrame();
   }
 
-
+} while (u8g2.nextPage());
 }
 //</Loop>
